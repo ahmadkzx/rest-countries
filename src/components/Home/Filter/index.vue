70ab17d55753d52128693b65237c4e4d117ecc45
Ahmad Karimzade <ahmadkarimzadedev@gmail.com>
2022-08-14 21:33:50 +0430

    feat: filter component ui

---
+++ src/components/Home/Filter/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="filter">
+    <div class="filter__current" @click="toggleList">
+      <span class="filter__current-title">Filter by Region</span>
+      <i class="filter__current-icon gg-chevron-down"></i>
+    </div>
+
+    <ul v-if="isShowList" class="filter__list">
+      <li class="filter__list-item">Africa</li>
+      <li class="filter__list-item">America</li>
+      <li class="filter__list-item">Asia</li>
+      <li class="filter__list-item">Europe</li>
+      <li class="filter__list-item">Oceania</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Filter',
+
+  data: () => ({
+    isShowList: false,
+  }),
+
+  methods: {
+    toggleList() {
+      this.isShowList = !this.isShowList
+    },
+  },
+}
+</script>
+
+<style src="./index.scss" lang="scss" scoped />
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="container">
     <SearchInput />
+    <Filter />
   </div>
 </template>
 
 <script>
+import Filter from '@/components/Home/Filter'
 import SearchInput from '@/components/Home/SearchInput'
 
 export default {
   name: 'Home',
 
   components: {
+    Filter,
     SearchInput,
   },
 }
