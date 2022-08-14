c968a935a7e0cff948c2989a902b034f3879b42b
Ahmad Karimzade <ahmadkarimzadedev@gmail.com>
2022-08-14 21:46:44 +0430

    feat: country card ui

---
+++ src/components/Home/CountryCard/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="country">
+    <img
+      src="https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/1200px-Flag_of_the_United_States.svg.png"
+      alt="America"
+      class="country__flag"
+    />
+
+    <div class="country__info">
+      <h5 class="country__info-name">United States Of America</h5>
+
+      <div class="country__info-row">
+        <span class="country__info-row-key">Population:</span>
+        <span class="country__info-row-value">80000000</span>
+      </div>
+
+      <div class="country__info-row">
+        <span class="country__info-row-key">Region:</span>
+        <span class="country__info-row-value">America</span>
+      </div>
+
+      <div class="country__info-row">
+        <span class="country__info-row-key">Capital:</span>
+        <span class="country__info-row-value">Washington</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CountryCard',
+}
+</script>
+
+<style src="./index.scss" lang="scss" scoped />
